949f1d4ef9fa5c059fd941f5d06600a46bf841ab
Aurelie Gedeon <agedeon540@gmail.com>
2024-07-04 00:43:13 -0500

    AG removed test get request; removed console log

--- P1/backend/index.js
+++ P1/backend/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const knex = require("knex");
 const knexFile = require("./knexfile");
-console.log(knexFile);
 const environment = process.env.NODE_ENV || "development";
 
 const db = knex(knexFile[environment]);
@@ -20,11 +19,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   console.log("Received a get request");
   res.send("Hey, your server is running.");
-});
-
-app.get("/hello", (req, res) => {
-  console.log("I'm saying hello!!");
-  res.send("I'm saying hello back!!!!");
 });
 
 app.post("/api/movies", async (req, res) => {
