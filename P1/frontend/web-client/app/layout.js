9fae72826e9c5ec2916d0174f36f16d50434d581
Karla Sanchez <karlaonrails@gmail.com>
2024-08-26 04:13:18 -0400

    Create layout.js
    updated default font to Outfit for entire application in <body> tags

---
+++ P1/frontend/web-client/app/layout.js
@@ -0,0 +1,17 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Team Boulders",
+  description: "Entertainment Web App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>{children}</body>
+    </html>
+  );
+}
